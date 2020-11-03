be4cef952062820fdbeed7d6a266304efa4cff3a
Tropical <42101043+Tropix126@users.noreply.github.com>
2020-11-03 13:18:58 -0600

    First pass at UI

---
+++ src/App.svelte
@@ -0,0 +1,63 @@
+<script>
+    import Page from './containers/Page.svelte';
+</script>
+
+<main class="installer-body">
+    <Page/>
+</main>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
+
+    :root {
+        --bg1: #040405;
+        --bg2: #0c0d10;
+        --bg2-alt: #101116;
+        --bg3: #14151b;
+        --bg3-alt: #191a21;
+        --bg4: #20212b;
+        --text-light: #f1f1f1;
+        --text-normal: #bfc4c9;
+        --text-muted: #95989d;
+        --accent: #3a71c1;
+        --accent-hover: #2f5b9d;
+    }
+
+    :global(*),
+    :global(*::after),
+    :global(*::before) {
+        box-sizing: border-box;
+        -webkit-user-drag: none;
+        font-family: "Inter", sans-serif;
+        user-select: none;
+        outline: none;
+    }
+
+    .installer-body {
+        overflow: hidden;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        z-index: 1;
+        padding: 20px;
+        background: radial-gradient(var(--bg2) 50%, var(--bg2-alt));
+        border-radius: 4px;
+        width: 500px;
+        height: 300px;
+    }
+
+    .installer-body::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url('../assets/images/background.png');
+        z-index: -1;
+        opacity: 0.35;
+        pointer-events: none;
+        mask: radial-gradient(transparent, #000);
+        -webkit-mask: radial-gradient(transparent, #000);
+    }
+</style>
---
+++ src/pages/License.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Header from '../common/Header.svelte';
+    import Spinner from '../common/Spinner.svelte';
+</script>
+
+<Header hasMargin>License</Header>
+<Spinner/>
