a72c39d17563545e8425f490501a9a4bf9acef67
Tropical <42101043+Tropix126@users.noreply.github.com>
2021-03-13 03:35:31 -0600

    open anchor tabs in browser

--- src/renderer/actions/quit.js
+++ src/renderer/actions/quit.js
@@ -9,7 +9,7 @@ export default async function() {
         buttons: ["Quit", "Cancel"]
     });
 
-    if (confirmation.response === 2) {
+    if (confirmation.response === 1) {
         remote.app.quit();
     }
 }
