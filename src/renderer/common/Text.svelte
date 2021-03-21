e322fc18a4995b51a49f073fb63b2a52be606618
Tropical <42101043+Tropix126@users.noreply.github.com>
2021-03-21 16:34:39 -0500

    (unfinished) header overhaul

---
+++ src/renderer/common/Text.svelte
@@ -0,0 +1,49 @@
+<script>
+    export let hasMargin;
+    export let type = "paragraph";
+</script>
+
+{#if type == "header"}
+    <h3 class="header {(hasMargin == true) ? "has-margin" : ""}">
+        <slot/>
+    </h3>
+{:else if type == "paragraph"}
+    <p class="paragraph {(hasMargin == true) ? "has-margin" : ""}">
+        <slot/>
+    </p>
+{:else if type == "subtext"}
+    <span class="subtext {(hasMargin == true) ? "has-margin" : ""}">
+        <slot/>
+    </span>
+{/if}
+
+<style>
+    .header,
+    .paragraph,
+    .subtext {
+        margin: 0;
+        line-height: normal;
+    }
+
+    .has-margin {
+        margin-bottom: 10px;
+    }
+
+    .header {
+        color: var(--text-light);
+        font-weight: 600;
+        font-size: 16px;
+    }
+
+    .paragraph {
+        color: var(--text-normal);
+        font-weight: 400;
+        font-size: 14px;
+    }
+
+    .subtext {
+        color: var(--text-muted);
+        font-weight: 400;
+        font-size: 12px;
+    }
+</style>
