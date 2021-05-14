c56880470227d53bae9dc9d7e97002bcd298d189
Jed Fox <git@jedfox.com>
2021-05-14 13:19:33 -0400

    Add a checkmark to the selected discord versions for clarity

--- src/renderer/common/Multiselect.svelte
+++ src/renderer/common/Multiselect.svelte
@@ -2,7 +2,7 @@
     import Button from "./Button.svelte";
     import {handleKeyboardToggle} from "../stores/controls.js";
     import {createEventDispatcher} from "svelte";
-    
+
     export let value;
     export let description;
     export let disabled = false;
@@ -32,6 +32,11 @@
         <div class="button" on:keypress={e => e.stopPropagation()}>
             <Button type="secondary" on:click={click}>Browse</Button>
         </div>
+        <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="24" height="24">
+            {#if checked}
+            <path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path>
+            {/if}
+        </svg>
     </div>
 </label>
 
@@ -56,14 +61,14 @@
     }
 
     .check-container:last-child {
-        margin: 0;  
+        margin: 0;
     }
 
     .check-item.disabled {
         background-color: var(--bg2-alt);
         cursor: not-allowed;
     }
-    
+
     .check-container input:checked + .check-item {
         background-color: var(--accent);
     }
@@ -135,5 +140,10 @@
 
     :global(.check-container input:checked + .check-item .btn:active) {
         opacity: .8;
+    }
+
+    .checkmark {
+        fill: white;
+        margin-left: 1em;
     }
 </style>
