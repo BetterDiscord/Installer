59c8556096361d9debc0557bdae6b0a4059e20d0
Jed Fox <git@jedfox.com>
2021-05-14 13:16:14 -0400

    Remove requirement to agree to the MIT license since that is not required

--- src/renderer/routes.js
+++ src/renderer/routes.js
@@ -1,12 +1,10 @@
 import LoadingPage from "./pages/Loading.svelte";
-import LicensePage from "./pages/License.svelte";
 import ActionsPage from "./pages/Actions.svelte";
 import PlatformsPage from "./pages/Platforms.svelte";
 import PerformActionPage from "./pages/PerformAction.svelte";
 
 export default {
-    "/": LicensePage,
-    "/actions": ActionsPage,
+    "/": ActionsPage,
     "/setup/:action": PlatformsPage,
     "/install": PerformActionPage,
     "/repair": PerformActionPage,
