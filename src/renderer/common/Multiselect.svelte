6795da60d43a92efe23222024c89a2fbc74b63b5
Jed Fox <git@jedfox.com>
2021-05-23 17:57:08 -0400

    Move checkmark before browse button

--- src/renderer/common/Multiselect.svelte
+++ src/renderer/common/Multiselect.svelte
@@ -29,14 +29,14 @@
             </h5>
             <span title={description}>{description}</span>
         </div>
-        <div class="button" on:keypress={e => e.stopPropagation()}>
-            <Button type="secondary" on:click={click}>Browse</Button>
-        </div>
         <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="24" height="24">
             {#if checked}
             <path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path>
             {/if}
         </svg>
+        <div class="button" on:keypress={e => e.stopPropagation()}>
+            <Button type="secondary" on:click={click}>Browse</Button>
+        </div>
     </div>
 </label>
 
@@ -144,6 +144,6 @@
 
     .checkmark {
         fill: white;
-        margin-left: 1em;
+        margin: 0 1em;
     }
 </style>
