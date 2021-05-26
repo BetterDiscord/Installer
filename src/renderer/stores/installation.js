0a91d9e65f56d6a3905bbfa0486850309d742d8b
Qb <qb@qbit.moe>
2021-05-26 22:33:24 +0200

    Automatically select the most stable detected version

--- src/renderer/stores/installation.js
+++ src/renderer/stores/installation.js
@@ -4,7 +4,18 @@ import readwritable from "./types/readwritable";
 
 export const status = writable("");
 export const hasAgreed = writable(false);
-export const platforms = writable({stable: false, canary: false, ptb: false});
+
+const defaultPlatforms = {stable: false, canary: false, ptb: false};
+if (locations.stable) {
+    defaultPlatforms.stable = true;
+}
+else if (locations.ptb) {
+    defaultPlatforms.ptb = true;
+}
+else if (locations.canary) {
+    defaultPlatforms.canary = true;
+}
+export const platforms = writable(defaultPlatforms);
 export const paths = writable({stable: locations.stable, canary: locations.canary, ptb: locations.ptb});
 
 export const progress = readwritable(0);
