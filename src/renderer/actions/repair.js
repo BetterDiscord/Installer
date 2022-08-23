42705cda8d7ae285464a58037b55751ef76483df
Bananaman <38923130+Bananaman@users.noreply.github.com>
2022-08-23 22:54:23 +0200

    repair.js: Complete rewrite of the repair code, adds Flatpak/Snap
    - Now uses the unified function to "find latest Discord version", which solves the previous issues of having differing implementations everywhere.
    
    - The "deleteAppDirs" function has been disabled on Linux, since it's completely irrelevant there. And its logging on Mac/Windows has been revised so that it doesn't output anything if there's nothing to delete.
    
    - The "deleteModuleDirs" function has been rewritten to support Linux, and to support Flatpaks and Snaps. Its logging has also been revised for all platforms so that it doesn't output anything if there's nothing to delete.
    
    - The "showInstallNotice" function has been completely rewritten to handle the fact that Discord *must* be started manually by the user to re-download its latest modules before we're able to install BetterDiscord again. The old "Repair" code was broken since it deleted Discord's modules-folders and then attempted to install BetterDiscord into those deleted folders. The new code warns the user about the situation and provides instructions for what they have to do, and won't let them proceed with the installation of BetterDiscord again until it has verified that the user has launched Discord manually (by checking that the installaton paths exist again).
    
    - Improved the popup message formatting, for greater clarity.
    
    - Improved the GUI's logging messages, with better explanations such as "exiting" instead of "killing", since non-technical users are the primary audience of BetterDiscord-Installer.
    
    - Added detailed comments for future maintainers/contributors.

--- src/renderer/actions/repair.js
+++ src/renderer/actions/repair.js
@@ -1,8 +1,7 @@
 import {progress} from "../stores/installation";
 import {remote} from "electron";
-import {promises as fs} from "fs";
+const fs = require("fs");
 import originalFs from "original-fs";
-import rimraf from "rimraf";
 import path from "path";
 import install from "./install.js";
 import {log, lognewline} from "./utils/log";
@@ -23,17 +22,18 @@ async function deleteShims(paths) {
     const progressPerLoop = (DELETE_SHIM_PROGRESS - progress.value) / paths.length;
     for (const discordPath of paths) {
         const indexFile = path.join(discordPath, "index.js");
-        log("Removing " + indexFile);
         try {
-            if (await exists(indexFile)) await fs.writeFile(indexFile, `module.exports = require("./core.asar");`);
-            log("✅ Deletion successful");
-            progress.set(progress.value + progressPerLoop);
+            if (await exists(indexFile)) {
+                log(`Removing shim: ${indexFile}.`);
+                await fs.writeFile(indexFile, `module.exports = require("./core.asar");`);
+                log("✅ Deletion successful.");
+            }
         }
         catch (err) {
-            log(`❌ Could not delete file ${indexFile}`);
+            log(`❌ Could not remove shim ${indexFile}.`);
             log(`❌ ${err.message}`);
-            return err;
         }
+        progress.set(progress.value + progressPerLoop);
     }
 }
 
@@ -63,55 +63,84 @@ async function disableAllPlugins(channels) {
     }
 }
 
-async function showInstallNotice(config) {
-    const confirmation = await remote.dialog.showMessageBox(remote.BrowserWindow.getFocusedWindow(), {
-        type: "question",
-        title: "Reinstall BetterDiscord?",
-        message: "After repairing, you need to reinstall BetterDiscord. Would you like to do that now?",
-        noLink: true,
-        cancelId: 1,
-        buttons: ["Yes", "No"]
-    });
+async function showInstallNotice(config, discordPaths) {
+    // Let the user know that they must launch Discord to download the "modules"
+    // folders that we deleted, otherwise we won't be able to inject since the
+    // files won't exist. If the user proceeds without reading, they'll get an error.
+    while (true) {
+        const confirmation = await remote.dialog.showMessageBox(remote.BrowserWindow.getFocusedWindow(), {
+            type: "question",
+            title: "Reinstall BetterDiscord?",
+            message: "After repairing, you need to reinstall BetterDiscord.\n\nBut first, you must manually launch all of your repaired Discord versions, so that they will download their modules again.\n\nWhen you are ready, return to this dialog and proceed with the BetterDiscord installation.\n\nAre you ready to install BetterDiscord now?",
+            noLink: true,
+            cancelId: 1,
+            buttons: ["Yes", "No"]
+        });
 
-    if (confirmation.response !== 0) return succeed();
+        if (confirmation.response !== 0) return succeed(); // User says no.
 
+        // User wants to reinstall BetterDiscord. Verify that all config-target paths
+        // exist again (to make sure that they have launched each repaired Discord version).
+        let installReady = true;
+        for (const discordPath of discordPaths) {
+            if (!(await exists(discordPath))) {
+                installReady = false;
+                await remote.dialog.showMessageBox({
+                    type: "error",
+                    title: "Please Launch Discord First",
+                    message: "BetterDiscord can't be reinstalled yet.\n\nPlease make sure to manually launch all of your repaired Discord versions, so that they will download their modules again."
+                });
+                break;
+            }
+        }
+
+        if (installReady) break;
+    }
+
+    // User wants to reinstall BetterDiscord, and all config-target paths exist.
+    // NOTE: Since the user had to launch each repaired Discord version (above),
+    // to download their "modules" again, they will most likely still be running
+    // while the installer below is triggered. The installer will then restart
+    // all of their running Discord instances automatically post-installation.
+    // So never tell the user to manually restart their Discord clients below!
     await reset();
     await install(config);
     remote.dialog.showMessageBox(remote.BrowserWindow.getFocusedWindow(), {
         type: "info",
-        title: "Reinstall Complete",
-        message: "Please relaunch discord manually to finish the repair."
+        title: "Repair Complete",
+        message: "Your chosen Discord versions have been repaired and are now ready for use."
     });
 }
-
 
 export default async function(config) {
     await reset();
     const sane = doSanityCheck(config);
     if (!sane) return fail();
 
-
+    // Installation channels (such as "stable", "canary") and their latest version's paths.
     const channels = Object.keys(config);
     const paths = Object.values(config);
 
 
-    lognewline("Killing Discord...");
-    const killErr = await kill(channels, (KILL_DISCORD_PROGRESS - progress.value) / channels.length, false); // await killProcesses(channels);
+    // Ensure that Discord isn't running.
+    lognewline("Exiting Discord...");
+    const killErr = await kill(channels, (KILL_DISCORD_PROGRESS - progress.value) / channels.length, false);
     if (killErr) {
         showKillNotice();
         return fail();
     }
-    log("✅ Discord Killed");
+    log("✅ Discord stopped.");
     progress.set(KILL_DISCORD_PROGRESS);
 
 
+    // Remove shims.
     await new Promise(r => setTimeout(r, 200));
     lognewline("Deleting shims...");
     const deleteShimErr = await deleteShims(paths);
     if (deleteShimErr) return fail();
-    log("✅ Shims deleted");
+    log("✅ Shims deleted.");
     progress.set(DELETE_SHIM_PROGRESS);
-    
+
 
     await new Promise(r => setTimeout(r, 200));
     lognewline("Disabling all plugins...");
@@ -121,5 +150,6 @@ export default async function(config) {
     progress.set(DELETE_PLUGINS_JSON_PROGRESS);
 
 
-    showInstallNotice(config);
+    // Tell the user that they need to reinstall BetterDiscord now.
+    showInstallNotice(config, paths);
 };
