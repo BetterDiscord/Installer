d047fc14b7a2838dea5fa2ecaa66cde4032e2ad7
Bananaman <38923130+Bananaman@users.noreply.github.com>
2022-08-23 22:40:36 +0200

    install.js: Complete rewrite of the installation code, adds Flatpak/Snap
    - Functions now take arguments rather than abusing global variables.
    
    - Fixed the BetterDiscord ASAR downloading code to handle missing URLs without crashing.
    
    - Implemented a better method of loading the BetterDiscord ASAR on Linux, via relative paths rather than hardcoding absolute paths. This makes the config portable between Native, Flatpak and Snap installations, and means that it won't break anymore if the user's home-folder is renamed or if they move the data to another computer/account.
    
    - Added support for installing BetterDiscord's ASAR into multiple folders at the same time (via the "bdFolders" variable that we now build). This new feature is used for the Linux Flatpak/Snap support, since we need to install BetterDiscord separately into their sandboxes.
    
    - The ASAR downloading process is now 2-3x faster, since it now saves it to a single, temporary file, and then copies that file into *all* Discord target paths (all Discord versions), so that it doesn't waste time downloading the exact same file over and over again for all destinations whenever the user has multiple Discord versions/paths on their system.
    
    - The code for migrating old BetterDiscord installations has been rewritten to support all platforms, not just Macs. This makes it easy to do future moves of the BD data folders on any platform.
    
    - Improved the GUI's logging messages, with better explanations instead of technical words such as "shims", since non-technical users are the primary audience of BetterDiscord-Installer.
    
    - Added detailed comments for future maintainers/contributors.

--- src/renderer/actions/install.js
+++ src/renderer/actions/install.js
@@ -1,8 +1,9 @@
 import {progress, status} from "../stores/installation";
 import {remote, shell} from "electron";
-import {promises as fs} from "fs";
+import {promises as fs, copyFileSync} from "fs";
 import path from "path";
 import phin from "phin";
+const tmp = require("tmp");
 
 import {log, lognewline} from "./utils/log";
 import succeed from "./utils/succeed";
@@ -20,27 +21,21 @@ const RESTART_DISCORD_PROGRESS = 100;
 
 const RELEASE_API = "https://api.github.com/repos/BetterDiscord/BetterDiscord/releases";
 
-const bdFolder = path.join(remote.app.getPath("appData"), "BetterDiscord");
-const bdDataFolder = path.join(bdFolder, "data");
-const bdPluginsFolder = path.join(bdFolder, "plugins");
-const bdThemesFolder = path.join(bdFolder, "themes");
-
-
 async function makeDirectories(...folders) {
     const progressPerLoop = (MAKE_DIR_PROGRESS - progress.value) / folders.length;
     for (const folder of folders) {
         if (await exists(folder)) {
-            log(`✅ Directory exists: ${folder}`);
+            log(`✅ Directory exists: ${folder}.`);
             progress.set(progress.value + progressPerLoop);
             continue;
         }
         try {
             await fs.mkdir(folder);
             progress.set(progress.value + progressPerLoop);
-            log(`✅ Directory created: ${folder}`);
+            log(`✅ Directory created: ${folder}.`);
         }
         catch (err) {
-            log(`❌ Failed to create directory: ${folder}`);
+            log(`❌ Failed to create directory: ${folder}.`);
             log(`❌ ${err.message}`);
             return err;
         }
@@ -100,8 +95,9 @@ async function downloadAsar() {
     }
 }
 
-const asarPath = path.join(bdDataFolder, "betterdiscord.asar");
-async function installAsar(fileContent) {
+async function installAsar(fileContent, bdFolder) {
+    const bdDataFolder = path.join(bdFolder, "data");
+    const asarPath = path.join(bdDataFolder, "betterdiscord.asar");
     try {
         const originalFs = require("original-fs").promises; // because electron doesn't like writing asar files
         await originalFs.writeFile(asarPath, fileContent);
@@ -113,27 +109,45 @@ async function installAsar(fileContent) {
     }
 }
 
-async function downloadAndInstallAsar() {
+async function downloadAndInstallAsar(bdFolders) {
     try {
         const fileContent = await downloadAsar();
-        await installAsar(fileContent);
+        for (const bdFolder of bdFolders) {
+            await installAsar(fileContent, bdFolder);
+        }
     } 
     catch (error) {
         return error;
     }
 }
 
-async function injectShims(paths) {
+async function injectShims(paths, bdFolders) {
+    // * Mac and Windows: All Discord installations share a single, absolute
+    //   folder path where BetterDiscord is installed (in "bdFolders[0]").
+    // * Linux: Every Discord installation has its own BetterDiscord folder,
+    //   and uses relative paths to load them, so we don't need "bdFolders".
+    //   Flatpaks/Snaps have individual per-Discord-channel BetterDiscord folders,
+    //   since every sandboxed installation has their own xdg-config structure,
+    //   whereas all native installations share the `~/.config/BetterDiscord` folder.
     const progressPerLoop = (INJECT_SHIM_PROGRESS - progress.value) / paths.length;
     for (const discordPath of paths) {
-        log("Injecting into: " + discordPath);
+        log(`Injecting into: ${discordPath}.`);
         try {
-            await fs.writeFile(path.join(discordPath, "index.js"), `require("${asarPath.replace(/\\/g, "\\\\").replace(/"/g, "\\\"")}");\nmodule.exports = require("./core.asar");`);
-            log("✅ Injection successful");
+            if (process.platform === "win32" || process.platform === "darwin") { // Windows and Mac.
+                const asarPath = path.join(bdFolders[0], "data", "betterdiscord.asar");
+                await fs.writeFile(path.join(discordPath, "index.js"), `require("${asarPath.replace(/\\/g, "\\\\").replace(/"/g, "\\\"")}");\nmodule.exports = require("./core.asar");`);
+            }
+            else { // Linux.
+                await fs.writeFile(
+                    path.join(discordPath, "index.js"),
+                    "require('../../../../BetterDiscord/data/betterdiscord.asar');\nmodule.exports = require('./core.asar');"
+                );
+            }
+            log("✅ Injection successful.");
             progress.set(progress.value + progressPerLoop);
         }
         catch (err) {
-            log(`❌ Could not inject shims to ${discordPath}`);
+            log(`❌ Could not inject loader for ${discordPath}.`);
             log(`❌ ${err.message}`);
             return err;
         }
@@ -146,36 +160,74 @@ export default async function(config) {
     const sane = doSanityCheck(config);
     if (!sane) return fail();
 
-
+    // Installation channels (such as "stable", "canary") and their latest version's paths.
     const channels = Object.keys(config);
     const paths = Object.values(config);
 
+    // Latest BD paths. This is where we'll install BetterDiscord's ASAR package.
+    const bdFolders = [];
+    if (process.platform === "win32" || process.platform === "darwin") {
+        // Windows and Mac: BetterDiscord stored in a single, shared location.
+        bdFolders.push(path.join(remote.app.getPath("appData"), "BetterDiscord"));
+    }
+    else { // Linux.
+        // NOTE: On Linux, we install BetterDiscord into every Discord instance's
+        // individual xdg-config directory, to be able to support Flatpaks and Snaps,
+        // and to avoid conflicts if multiple sandboxed Discord versions run at the
+        // same time. Only the sandboxes have truly unique "per-installation" BD paths.
+        for (const discordPath of paths) {
+            // Jump out of the latest Discord version's subdirectory, to the Discord
+            // installation's main xdg-config directory, and install BetterDiscord there.
+            // Before: "/home/foo/.var/app/com.discordapp.Discord/config/discord/0.0.19/modules/discord_desktop_core".
+            // After: "/home/foo/.var/app/com.discordapp.Discord/config/BetterDiscord".
+            // NOTE: Installation paths on Linux always contain those 4 suffix parts.
+            // NOTE: The requirement that BetterDiscord is installed in xdg-config
+            // is hardcoded into betterdiscord.asar, so we can't change these paths.
+            bdFolders.push(path.join(discordPath, "../../../..", "BetterDiscord"));
+        }
+    }
 
+    if (bdFolders.length < 1) {
+        lognewline("No Discord paths provided.");
+        return fail();
+    }
+
+
+    // Create BetterDiscord's directory hierarchies.
     lognewline("Creating required directories...");
-    const makeDirErr = await makeDirectories(bdFolder, bdDataFolder, bdThemesFolder, bdPluginsFolder);
-    if (makeDirErr) return fail();
-    log("✅ Directories created");
+    for (const bdFolder of bdFolders) {
+        const makeDirErr = await makeDirectories(
+            bdFolder,
+            path.join(bdFolder, "data"),
+            path.join(bdFolder, "plugins"),
+            path.join(bdFolder, "themes")
+        );
+        if (makeDirErr) return fail();
+    }
+    log("✅ Directories created.");
     progress.set(MAKE_DIR_PROGRESS);
     
 
     lognewline("Downloading asar file");
-    const downloadErr = await downloadAndInstallAsar();
+    const downloadErr = await downloadAndInstallAsar(bdFolders);
     if (downloadErr) return fail();
-    log("✅ Package downloaded");
+    log("✅ Package downloaded.");
     progress.set(DOWNLOAD_PACKAGE_PROGRESS);
 
 
-    lognewline("Injecting shims...");
+    // Add BetterDiscord to each Discord version's loader-script.
+    lognewline("Injecting into Discord...");
     const injectErr = await injectShims(paths);
     if (injectErr) return fail();
-    log("✅ Shims injected");
+    log("✅ Loader scripts injected.");
     progress.set(INJECT_SHIM_PROGRESS);
 
 
+    // Automatically restart Discord clients if they are running.
     lognewline("Restarting Discord...");
     const killErr = await kill(channels, (RESTART_DISCORD_PROGRESS - progress.value) / channels.length);
-    if (killErr) showRestartNotice(); // No need to bail out and show failed
-    else log("✅ Discord restarted");
+    if (killErr) showRestartNotice(); // No need to bail out if we failed, just tell user to restart manually.
+    else log("✅ Discord restarted.");
     progress.set(RESTART_DISCORD_PROGRESS);
 
 
