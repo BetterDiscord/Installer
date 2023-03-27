b587fd422d60f0d939dbf3e7aec70eee0ced6d75
Zerebos <rauenzi@outlook.com>
2023-03-27 09:30:56 -0400

    Fix webpack builds for modern versions (#333)

--- webpack.renderer.js
+++ webpack.renderer.js
@@ -1,13 +1,9 @@
-module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.(html|svelte)$/,
-                use: "svelte-loader"
-              }
-        ]
-    },
-    output: {
-        hashFunction: "xxhash64"
-    }
+module.exports = config => {
+    delete config.optimization.namedModules;
+    config.output.hashFunction = "xxhash64";
+    config.module.rules.push({
+        test: /\.(html|svelte)$/,
+        use: "svelte-loader"
+    });
+    return config;
 };
