c454600020941c3a01a835b2658717f891cfe8ff
moongazer07 <myboymohammed@gmail.com>
2023-05-22 19:12:20 -0400

    Update installation.js

--- src/renderer/stores/installation.js
+++ src/renderer/stores/installation.js
@@ -4,8 +4,8 @@ import readwritable from "./types/readwritable";
 
 export const status = writable("");
 export const hasAgreed = writable(false);
-export const platforms = writable({stable: false, canary: false, ptb: false});
-export const paths = writable({stable: locations.stable, canary: locations.canary, ptb: locations.ptb});
+export const platforms = writable({stable: false, canary: false, development: false, ptb: false});
+export const paths = writable({stable: locations.stable, canary: locations.canary, development: locations.development, ptb: locations.ptb});
 
 export const progress = readwritable(0);
 export const action = readwritable("install");
